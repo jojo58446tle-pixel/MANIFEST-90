0b134689e7b3c151b3bf874b9b243820fd1f397c
jojo58446tle-pixel <jojo58446tle@gmail.com>
2026-08-10 15:13:24 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-const CACHE='manifest90-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/styles.css','/app.js','/manifest.webmanifest']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='manifest90-v5';
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['/index.html','/styles.css','/app.js','/manifest.webmanifest'])).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET'||event.request.url.includes('/.netlify/functions/'))return;
+  event.respondWith(fetch(event.request).then(response=>{let copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('/index.html'))));
+});
